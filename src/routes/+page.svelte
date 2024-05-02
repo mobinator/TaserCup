5be4df795210a04e49be45d72b68d84ed9f7fede
Toni <toni.barsties@gmail.com>
2024-05-02 14:20:55 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { goto } from '$app/navigation';
+    
+    function navigateToGame() {
+      goto('/game');
+    }
+  </script>
+  
+  <style>
+    
+    main {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+      background-color: #ffad3b;
+    }
+    
+    img {
+      margin-bottom: 4rem;
+    }
+  
+    button {
+      background-color: white;
+      border: none;
+      padding: 0.8em 6em;
+      border-radius: 100px;
+      font-size: 1.5rem;
+      cursor: pointer;
+      transition: background-color 0.3s;
+      font-family: 'Poppins', sans-serif; /* Add this line */
+      font-weight: 900; /* Add this line */
+    }
+    
+    button:hover {
+      background-color: #f0f0f0;
+    }
+  </style>
+  
+  <main>
+    <img src="/game.svg" alt="Game Logo">
+    <button on:click={navigateToGame}>Start</button>
+  </main>
+  
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+  let name = "Tasse 1";
+</script>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    background-color: #ffad3b;
+  }
+
+  .card {
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 15px;
+    padding: 20px;
+    width: 300px;
+    margin: auto;
+    text-align: center;
+  }
+
+  h1 {
+    margin-top: 1em;
+    font-size: 50pt;
+    color: white;
+  }
+</style>
+
+<main>
+  <h1>Game Page</h1>
+  <div class="card">
+    <h2 contenteditable="true" bind:innerText={name}>Enter Name</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet semper lorem. Sed feugiat, massa a finibus aliquet, nisl nunc cursus lorem, vitae aliquam massa ante non ex.</p>
+  </div>
+</main>
